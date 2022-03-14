f803fabdd6d0e5223f195b4e8a92030b749ddc82
TangoBee <71539314+TangoBeee@users.noreply.github.com>
2022-03-14 20:01:17 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,14 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Decode this: 5269636B204173746C6579",
+    answer: "Rick Astley",
+    options: [
+      "Vladimir Putin",
+      "Rick Astley",
+      "Linus Sebastian",
+      "Osama bin Laden"
+    ]
+  },
+];
